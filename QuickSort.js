a570c834b3d34d689c01378df1d9a36b979bb2c1
sjin8759 <sjin8759@uni.sydney.edu.au>
2020-09-29 17:19:37 +0800

    Improve quick sort

--- QuickSort.js
+++ QuickSort.js
@@ -17,7 +17,6 @@ function quickSort1(arr, left, right){
     }
     return arr;
 }
-
 function quickSort2(arr, left, right){
     if(left <= right){
         let i = left;
@@ -65,7 +64,27 @@ function quickSort3(arr, left, right){
     }
     return arr;
 }
+function quickSort4(arr, left, right){
+    while(left <= right){
+        let i = left;
+        let j = right;
+        let temp = arr[left];
+        while(i < j){
+            while(i < j && arr[j] >= temp)
+                j--;
+            while(i < j && arr[i] <= temp)
+                i++;
+            if (i < j)
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        [arr[left], arr[i]] = [arr[i], arr[left]];
+        quickSort1(arr, left, i - 1);
+        left = i + 1;
+    }
+    return arr;
+}
 let arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
 console.log(quickSort1(arr.slice(0), 0, arr.length - 1));
 console.log(quickSort2(arr.slice(0), 0, arr.length - 1));
 console.log(quickSort3(arr.slice(0), 0, arr.length - 1));
+console.log(quickSort4(arr.slice(0), 0, arr.length - 1));
